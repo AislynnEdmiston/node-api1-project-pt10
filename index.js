572be792bb11b2a8b58fc7b9564ff12f1485316c
AislynnEdmiston <davidandaislynn@gmail.com>
2020-02-11 22:31:10 -0500

    set up basic server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require("express");
+
+const server = express()
+
+server.use(express.json())
+
+
+server.listen(5000, () => {
+    console.log("Server is running at http://localhost:5000")
+})
